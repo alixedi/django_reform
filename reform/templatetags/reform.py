d9965379259794dd79e766b3c5b56794fda48303
Ali Zaidi <alixedi@bitswits.com>
2013-12-10 13:46:25 +0500

    Added support for controlling includes and multi-column layouts

--- reform/templatetags/reform.py
+++ reform/templatetags/reform.py
@@ -1,10 +1,14 @@
 from django import template
 from django.template.loader import get_template
-from django.forms import BaseForm
-from django.forms.forms import BoundField
-from django.conf import settings
+import itertools
+
 
 register = template.Library()
+
+def grouper(n, iterable, fillvalue=None):
+    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
 
 @register.filter
 def get_form_field_type(field):
@@ -12,24 +16,32 @@ def get_form_field_type(field):
     return field.field.__class__.__name__
 
 @register.simple_tag(takes_context=True)
-def reform(context, form_or_field, layout='vertical,false'):
+def reform(context, form, include=None, layout='vertical,false,1'):
     """This is my rendition of the as_bootstrap filter from the 
     bootstrap_toolkit as tag because I want to take in the perms parameter
     and pass it on to the bootstrap templates for use in rendering
     fields."""
     params = layout.split(',')
     layout = str(params[0]).lower()
+    columns = int(params[2])
+    if include is None:
+        ifields = [f for f in form]
+    else:
+        include = [f.strip() for f in include.split(',')]
+        ifields = []
+        for ifield in include:
+            for field in form:
+                if field.name == ifield:
+                    ifields.append(field)
     try:
         bootstrap_float = str(params[1]).lower() == 'float'
     except IndexError:
         bootstrap_float = False
-    context['form'] = form_or_field
+    ifields = grouper(columns, ifields)
+    colspan = int(12/columns)
+    context['form'] = form
+    context['ifields'] = ifields
+    context['colspan'] = colspan
     context['layout'] = layout
     context['float'] = bootstrap_float
-    if isinstance(form_or_field, BaseForm):
-        return get_template('bootstrap_toolkit/form.html').render(context)
-    elif isinstance(form_or_field, BoundField):
-        return get_template("bootstrap_toolkit/field.html").render(context)
-    else:
-        # Display the default
-        return settings.TEMPLATE_STRING_IF_INVALID
+    return get_template('bootstrap_toolkit/form.html').render(context)
